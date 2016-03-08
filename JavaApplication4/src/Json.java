997a38edf573399040ecfc39f9d40fc40f06b60f
reivaj94 <hectorjavier.moreno@unet.edu.ve>
2016-03-08 12:46:00 -0430

    trabajando en codificaciones json y decodificaciones

--- JavaApplication4/src/Json.java
+++ JavaApplication4/src/Json.java
@@ -12,25 +12,99 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import org.json.JSONObject;
+import static sun.management.Agent.error;
+
 /**
  *
  * @author javier
  */
 public class Json {
-      JsonObject paquete = new JsonObject();
-      String cadena_JSON;
-    
-    public String code_1(Object nombre, Object tiempo,Object espacios){
+
+    JsonObject paquete = new JsonObject();
+    String cadena_JSON;
+
+    public String code_1(Object nombre, Object tiempo, Object espacios) {
         paquete.addProperty("COD", "1");
         paquete.addProperty("NOMBRE", nombre.toString());
         paquete.addProperty("TIEMPO", tiempo.toString());
         paquete.addProperty("ESPACIOS", espacios.toString());
         cadena_JSON = paquete.toString();
-        paquete.remove("COD");
-        paquete.remove("NOMBRE");
-        paquete.remove("TIEMPO");
-        paquete.remove("ESPACIOS");
-        
+
         return cadena_JSON;
+    }
+
+    public Object deco_1(String JSON, int opcion) {
+        JsonParser parser = new JsonParser();
+        JsonElement Objeto = parser.parse(JSON);
+
+        if (opcion == 0) {
+            return Objeto.getAsJsonObject().get("COD").getAsInt();
+        }
+        if (opcion == 1) {
+            return Objeto.getAsJsonObject().get("NOMBRE").getAsString();
+        }
+        if (opcion == 2) {
+            return Objeto.getAsJsonObject().get("TIEMPO").getAsString();
+        }
+        if (opcion == 3) {
+            return Objeto.getAsJsonObject().get("ESPACIOS").getAsString();
+        } else {
+            System.err.println("error");
+        }
+
+        return null;
+    }
+
+    public String code_2(Object nombre) {
+        paquete.addProperty("COD", "2");
+        paquete.addProperty("NOMBRE", nombre.toString());
+        cadena_JSON = paquete.toString();
+
+        return cadena_JSON;
+    }
+
+    public Object deco_2(String JSON, int opcion) {
+        JsonParser parser = new JsonParser();
+        JsonElement Objeto = parser.parse(JSON);
+
+        if (opcion == 0) {
+            return Objeto.getAsJsonObject().get("COD").getAsInt();
+        }
+        if (opcion == 1) {
+            return Objeto.getAsJsonObject().get("NOMBRE").getAsString();
+        } else {
+            System.err.println("error");
+        }
+
+        return null;
+    }
+
+    public String code_3(Object acpt,Object dir,Object id) {
+        paquete.addProperty("COD", "2");
+        paquete.addProperty("ACEPTADO", acpt.toString());
+        paquete.addProperty("DIRECCION", dir.toString());
+        paquete.addProperty("ID", id.toString());
+        cadena_JSON = paquete.toString();
+
+        return cadena_JSON;
+    }
+    
+     public Object deco_3(String JSON, int opcion) {
+        JsonParser parser = new JsonParser();
+        JsonElement Objeto = parser.parse(JSON);
+
+        if (opcion == 0) 
+            return Objeto.getAsJsonObject().get("COD").getAsInt();
+        if (opcion == 1) 
+            return Objeto.getAsJsonObject().get("ACEPTADO").getAsString();
+        if (opcion == 2) 
+            return Objeto.getAsJsonObject().get("DIRECCION").getAsString();
+        if (opcion == 3) 
+            return Objeto.getAsJsonObject().get("ID").getAsString();
+        else 
+            System.err.println("error");
+        
+
+        return null;
     }
 }
