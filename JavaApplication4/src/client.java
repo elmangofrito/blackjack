d6da00bebd7b0ced6a5ba0d00c6fca7b2a1a7fdc
reivaj94 <hectorjavier.moreno@unet.edu.ve>
2016-03-09 09:29:44 -0430

    trabajando en codificaciones json y decodificaciones

--- JavaApplication4/src/client.java
+++ JavaApplication4/src/client.java
@@ -30,26 +30,30 @@ public class client implements Runnable {
 
     @Override
     public void run() {
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        byte[] receiveData = new byte[1024];
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        ul = new JComboBox();
-
-        //      while (true) {
-        Json x = new Json();
         try {
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            byte[] receiveData;
+            DatagramPacket receivePacket;
+            ul = new JComboBox();
             DatagramSocket clientSocket = new DatagramSocket(8050);
-            clientSocket.receive(receivePacket);
-            String modifiedSentence = new String(receivePacket.getData()).trim();
-            addserver(x.deco_1(modifiedSentence, 1).toString());
-            clientSocket.close();
+            Json x = new Json();
+            String modifiedSentence;
+            while (true) {
+                receiveData = new byte[1024];
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
+                modifiedSentence = new String(receivePacket.getData()).trim();
+                
+                if(modifiedSentence!=null)
+                    addserver(x.deco_1(modifiedSentence, 1).toString());
+                
+            }
         } catch (SocketException ex) {
             Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    //}
 
     public void addserver(String msg) {
         boolean in = false;
